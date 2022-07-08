8de2bf0d5bf62445d9f02ba4a7a6f980489bb663
hidayatbizmap <107534037+hidayatbizmap@users.noreply.github.com>
2022-07-08 15:38:01 +0530

    Delete news_management/public/js directory

--- news_management/public/js/quiz_questions.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Hidayatali and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Quiz Questions', {
-    published(frm) {
-        frm.set_value("date", frappe.datetime.now_datetime());
-    }
-})
