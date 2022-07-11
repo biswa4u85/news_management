0d1a4264c00f1e03e2fca5154f3ab23b3f7adb43
flash <flash@ip-172-31-3-10.ap-south-1.compute.internal>
2022-07-11 08:29:27 +0000

    Add news_category

---
+++ news_management/news_management/doctype/news_category/news_category.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Biswajit Sahoo and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class NewsCategory(NestedSet):
+	pass
