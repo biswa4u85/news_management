876fee373838ed55c71100121eec59ed116e9217
hidayatbizmap <107534037+hidayatbizmap@users.noreply.github.com>
2022-07-11 13:11:46 +0530

    Update blog_sub_category_table.py

--- news_management/utils/blog_sub_category_table.py
+++ news_management/utils/blog_sub_category_table.py
@@ -6,7 +6,9 @@ def fetch_data(doctype, txt, searchfield, start, page_len, filters):
   return frappe.db.sql("""
                       select category from `tabBlog Sub Category Table` where parent = "{0}"
                       """.format(filters.get("blog_sub_category")))  
-  
+ 
+#   Sub To Sub To Sub Category Data Filter
+
 @frappe.whitelist()
 def fetch_sub_category(doctype, txt, searchfield, start, page_len, filters):
   return frappe.db.sql("""
