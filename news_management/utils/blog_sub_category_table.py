45e67475ed093f05fedea3dc422a2e3304739024
hidayatbizmap <107534037+hidayatbizmap@users.noreply.github.com>
2022-07-11 11:38:52 +0530

    new Sub Category Filter

--- news_management/utils/blog_sub_category_table.py
+++ news_management/utils/blog_sub_category_table.py
@@ -7,7 +7,12 @@ def fetch_data(doctype, txt, searchfield, start, page_len, filters):
                       select category from `tabBlog Sub Category Table` where parent = "{0}"
                       """.format(filters.get("blog_sub_category")))  
   
-
+@frappe.whitelist()
+def fetch_sub_category(doctype, txt, searchfield, start, page_len, filters):
+  return frappe.db.sql("""
+                      select category from `tabBlog Sub To Sub Category Table` where parent = "{0}"
+                      """.format(filters.get("blog_sub_to_category")))  
+  
 
 
 
